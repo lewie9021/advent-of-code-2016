4ae3aff59f7381c3c43ed2b83fc138d0c47e9ff5
Lewis Barnes <lb.barnes@live.co.uk>
2017-09-05 21:59:39 +0100

    Completed Day 5, Part 2.

--- solutions/day-5/index.js
+++ solutions/day-5/index.js
@@ -11,22 +11,56 @@ const md5 = (value) => {
         .digest("hex");
 }
 
-const createPassword = (doorID, characterCount) => {
+function* generateHash(doorID, predicate) {
+    let index = 0;
+
+    while (true) {
+        const hash = md5(doorID + index);
+
+        if (predicate(hash))
+            yield hash;
+
+        index += 1;
+    }
+
+}
+
+const createSimplePassword = (doorID, characterCount) => {
+    const generator = generateHash(doorID, (hash) => {
+        return _.startsWith(hash, "00000")
+    });
     let password = "";
-    let index = -1;
 
     _.times(characterCount, () => {
-        while (true) {
-            index += 1;
-            const hash = md5(doorID + index);
+        const hash = generator.next().value;
 
-            if (!_.startsWith(hash, "00000"))
-                continue;
+        password += hash[5];
+    });
 
-            password += hash[5];
-            break;
-        }
+    return password;
+}
 
+const createComplexPassword = (doorID, characterCount) => {
+    let password = _.repeat("_", characterCount);
+    const generator = generateHash(doorID, (hash) => {
+        if (!_.startsWith(hash, "00000"))
+            return false;
+
+        const position = parseInt(hash[5])
+
+        // Ensure the position is a valid index.
+        if (!(position >= 0 && position <= (characterCount - 1)))
+            return false;
+
+        return password[position] == "_";
+    });
+
+    _.times(characterCount, () => {
+        const hash = generator.next().value;
+        const position = parseInt(hash[5]);
+        const value = hash[6];
+
+        password = password.substr(0, position) + value + password.substr(position + 1);
     });
 
     return password;
@@ -37,13 +71,12 @@ const run = () => {
     const input = FS.readFileSync(Path.join(__dirname, "input.txt"), "utf8");
     const doorID = input.trim();
 
-    console.log("Part 1:", createPassword(doorID, 8)); // d4cd2ee1
-    console.log("Part 2:"); //
+    console.log("Part 1:", createSimplePassword(doorID, 8)); // d4cd2ee1
+    console.log("Part 2:", createComplexPassword(doorID, 8)); // f2c730e5
 };
 
-run();
-
 module.exports = {
-    createPassword,
+    createSimplePassword,
+    createComplexPassword,
     run
 };
--- solutions/day-5/index.spec.js
+++ solutions/day-5/index.spec.js
@@ -1,19 +1,25 @@
 const { describe, it } = require("mocha");
 const { expect } = require("chai");
 
-const { createPassword } = require("./");
+const { createSimplePassword, createComplexPassword } = require("./");
 
-describe("Day 4:", () => {
+describe("Day 5:", function() {
 
-    describe("Part 1:", () => {
+    describe("Part 1:", function() {
 
-        it("returns '18f47a30', given a door ID of 'abc'", () => {
-            expect(createPassword("abc", 8)).to.eq("18f47a30");
+        it("returns '18f47a30', given a door ID of 'abc'", function() {
+            this.timeout(0);
+            expect(createSimplePassword("abc", 8)).to.eq("18f47a30");
         });
 
     });
 
-    describe("Part 2:", () => {
+    describe("Part 2:", function() {
+
+        it("returns '05ace8e3', given a door ID of 'abc'", function() {
+            this.timeout(0);
+            expect(createComplexPassword("abc", 8)).to.eq("05ace8e3");
+        });
 
     });
 
